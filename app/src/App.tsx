0d1652bd005540d6a9a0c98bfa9fb771de39cf7e
Spanfile <spansdev@gmail.com>
2020-03-23 18:08:43 +0200

    Initial commit

---
+++ app/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Container from 'react-bootstrap/Container';
+
+function App() {
+  return (
+    <div className="App">
+      <Container>
+      </Container>/
+    </div>
+  );
+}
+
+export default App;
